ae15ada1066729233e0024b8397429a8b43a5ecd
Jorge Lopez Querol <jorge@mjolnir.local>
2023-01-23 22:29:21 +0100

    empezamos

---
+++ src/eclipsedaw/main.java
@@ -0,0 +1,32 @@
+package eclipsedaw;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.print("natalia");
+		System.out.print("lizbeth");
+		System.out.print("miles");
+		System.out.print("gabriela");
+		System.out.print("pablo matías");
+		System.out.print("pablo");
+		System.out.print("david");
+		System.out.print("óscar");
+		System.out.print("fernando");
+		System.out.print("angele");
+		System.out.print("hugo");
+		System.out.print("josé luis");
+		System.out.print("miguel ángel");
+		System.out.print("josé antonio");
+		System.out.print("juan");
+		System.out.print("daniel");
+		System.out.print("alexandre");
+		System.out.print("abdullah");
+		System.out.print("motahare");
+		System.out.print("ricardo raúl");
+		System.out.print("manuel");
+		System.out.print("mario");
+		
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jorge
+ *
+ */
+module eclipsedaw {
+}
